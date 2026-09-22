893d847fc8db394bcd719400e73b5eae2545992b
Muhammad Mohid Khan <mohidk4202@gmail.com>
2026-09-22 14:23:36 -0400

    Data Types Changes

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,6 +1,6 @@
 public class DataTypes {
     public static long sum(int[] numbers) {
-        long s = 0; // variable to accumulate the sum in!
+        long s = 0;  // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
